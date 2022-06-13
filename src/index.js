56c772672953710e161e67edf526a6be0c90cd4f
jolega <alonso_juancho@hotmail.com>
2022-06-13 10:39:14 -0500

    peticion fetch

--- src/index.js
+++ src/index.js
@@ -4,10 +4,13 @@ const apikey = 'L9R3qzXtQJ6cV40iH6t6Q2eotz6DhnbK';
 const request = fetch (`https://api.giphy.com/v1/gifs/random?api_key=${ apikey }`)
 
 
-request.then ( resp => {
-    console.log(resp) ;
-    resp.json().then( data => {
-        console.log( data ) ;
-    })
-})
-.catch( console.warn );
+request
+       .then ( resp => resp.json() )
+       .then ( ({ data }) => {
+       const { url } = data.images.original
+       console.log(url)
+       const img = document.createElement('img')
+       img.src = url ;
+       document.body.append ( img ) ;
+       })
+     .catch( console.warn );
